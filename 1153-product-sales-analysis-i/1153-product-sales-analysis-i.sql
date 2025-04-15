8b36bbcd90940bab0146e176c903768405f950da
Kamaleshwaran A <kamaleshathappan@gmail.com>
2025-04-15 11:33:16 +0530

    Time: 1232 ms (84.04%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,8 +1,3 @@
-SELECT 
-    p.product_name, 
-    s.year, 
-    s.price
-FROM 
-    Sales s
-inner JOIN 
-    Product p ON s.product_id = p.product_id;
+select p.product_name,s.year,s.price from
+sales s
+inner join product p  on s.product_id=p.product_id
